dffd33f4a5fc849bcf3e58dcecb7e41f743af1a5
emjb2 <eboot123@gmail.com>
2023-04-20 00:55:28 +0100

    Code cleanup

--- analysis/plot_voxels.py
+++ analysis/plot_voxels.py
@@ -4,7 +4,6 @@ from general.pos_to_coords import pos_to_coords
 from math import floor
 
 def plot_voxels(n, lattice):
-    #limit = int(floor((max([len(x) for x in lattice])-min([len(x) for x in lattice]))/2+min([len(x) for x in lattice])))
     coordinates = np.zeros((n, n, max([len(x) for x in lattice])))
     colours = np.zeros((n, n, max([len(x) for x in lattice]), 4))
     for x in range(len(lattice)):
@@ -22,19 +21,6 @@ def plot_voxels(n, lattice):
                 colours[x_coord, y_coord, z_coord, 2] = 1
                 colours[x_coord, y_coord, z_coord, 3] = 0.6
 
-            #yd = (x_coord<x<x_coord+1) & (y_coord<y<y_coord+1) & (i<z<i+1)
-            #cube_list = cube_list + [[(x_coord<x<x_coord+1) & (y_coord<y<y_coord+1) & (i<z<i+1)]]
-            #if lattice[x][i] == 1:
-            #    cube_colours = cube_colours + ['red']
-            #else:
-            #    cube_colours = cube_colours + ['blue']
-
-    #colors = np.empty(cubes.shape, dtype=object)
-    #for x in range(len(cube_list)):
-    #    colors[cube_list[x][0]] = cube_colours[x]
-    #ax = plt.figure().add_subplot(projection = '3d')
-    #ax.voxels(cubes, facecolors = colors, edgecolor = 'k')
-    #plt.show()
     ax = plt.figure().add_subplot(projection='3d')
     ax.voxels(coordinates, facecolors = colours)
     ax.set_box_aspect([1, 1, max([len(x) for x in lattice])/n])    
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from simulation.make_plot_deltamu import make_plot_deltamu
-from simulation.run_simulation import run_simulation
 from simulation.make_plot_ratio import make_plot_ratio
-from simulation.make_plot_timesteps import make_plot_timesteps
-from analysis.plot_voxels import plot_voxels
 
 if __name__ == "__main__":
-    #make_plot_deltamu(30, [0,5,10], 1, 0.1, 290, 50000, 7)
-    #x = run_simulation(30, 7, 1, 0.1, 290, 50000)
     make_plot_ratio(30, 1, [1], 0, 0.1, 0.005, 290, 10000, 3)
-    #run_simulation(30, 4, 0.5, 0.05, 290, 20000)
